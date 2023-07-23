71a66f8cd296f1d6d5a06c0e7379465126f4a593
tsch0hnnny <tschohnnny@gmail.com>
2023-07-23 17:38:41 +0200

    work on preview

--- hooksandhighs.com/frontend/tailwind.config.js
+++ hooksandhighs.com/frontend/tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+      colors: {
+          'purple': '#7b2c80',
+          'deep-purple': '#3a153d',
+          'deeper-purple': '#2c102e',
+          'barely-purple': '#564356',
+          'barely-purple-light': '#d2c6d2',
+          'barely-purple-dark': '#433743',
+          'green': '#3daa75',
+          'black': '#171614',
+          'white': '#F7FFF6',
+          'cool-gray': '#7D8CA3',
+      },
+      extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    /** require('@tailwindcss/forms'), */
+    /** require('@tailwindcss/aspect-ratio'), */
+    /** require('@tailwindcss/container-queries'),*/
+  ],
 }
 
